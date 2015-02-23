44a05cdfdb48bdaaf124e83fcfaffafac48d27a4
Derek Sivers <derek@sivers.org>
2015-02-23 14:41:06 +1300

    NOTIFY trigger on sivers.comments

--- sivers/schema.sql
+++ sivers/schema.sql
@@ -19,9 +19,24 @@ CREATE INDEX compers ON comments(person_id);
 
 COMMIT;
 
+CREATE FUNCTION comments_changed() RETURNS TRIGGER AS $$
+DECLARE
+	u text;
+BEGIN
+	IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
+		u := NEW.uri;
+	ELSE
+		u := OLD.uri;
+	END IF;
+	PERFORM pg_notify('comments_changed', u);
+	RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER comments_changed AFTER INSERT OR UPDATE OR DELETE ON comments FOR EACH ROW EXECUTE PROCEDURE comments_changed();
+
 -- POST %r{^/comments/([0-9]+)$}
 -- PARAMS: comment id
-CREATE FUNCTION get_comment(integer, OUT mime text, OUT js json) AS $$
+CREATE OR REPLACE FUNCTION get_comment(integer, OUT mime text, OUT js json) AS $$
 BEGIN
 	mime := 'application/json';
 	js := row_to_json(r) FROM
@@ -43,7 +58,7 @@ $$ LANGUAGE plpgsql;
 
 -- POST %r{^/comments/([0-9]+)$}
 -- PARAMS: uri, name, email, html
-CREATE FUNCTION add_comment(text, text, text, text, OUT mime text, OUT js json) AS $$
+CREATE OR REPLACE FUNCTION add_comment(text, text, text, text, OUT mime text, OUT js json) AS $$
 DECLARE
 	new_uri text;
 	new_name text;
@@ -90,7 +105,7 @@ $$ LANGUAGE plpgsql;
 
 -- PUT %r{^/comments/([0-9]+)$}
 -- PARAMS: comments.id, JSON of values to update
-CREATE FUNCTION update_comment(integer, json, OUT mime text, OUT js json) AS $$
+CREATE OR REPLACE FUNCTION update_comment(integer, json, OUT mime text, OUT js json) AS $$
 DECLARE
 
 	err_code text;
@@ -123,7 +138,7 @@ $$ LANGUAGE plpgsql;
 
 -- POST %r{^/comments/([0-9]+)/reply$}
 -- PARAMS: comment_id, my reply
-CREATE FUNCTION reply_to_comment(integer, text, OUT mime text, OUT js json) AS $$
+CREATE OR REPLACE FUNCTION reply_to_comment(integer, text, OUT mime text, OUT js json) AS $$
 BEGIN
 	UPDATE sivers.comments SET html = CONCAT(html, '<br><span class="response">',
 		replace($2, ':-)',
@@ -138,7 +153,7 @@ $$ LANGUAGE plpgsql;
 
 -- DELETE %r{^/comments/([0-9]+)$}
 -- PARAMS: comment_id
-CREATE FUNCTION delete_comment(integer, OUT mime text, OUT js json) AS $$
+CREATE OR REPLACE FUNCTION delete_comment(integer, OUT mime text, OUT js json) AS $$
 DECLARE
 
 	err_code text;
@@ -170,7 +185,7 @@ $$ LANGUAGE plpgsql;
 
 -- DELETE %r{^/comments/([0-9]+)/spam$}
 -- PARAMS: comment_id
-CREATE FUNCTION spam_comment(integer, OUT mime text, OUT js json) AS $$
+CREATE OR REPLACE FUNCTION spam_comment(integer, OUT mime text, OUT js json) AS $$
 DECLARE
 	pid integer;
 
@@ -205,7 +220,7 @@ $$ LANGUAGE plpgsql;
 
 -- GET '/comments/new'
 -- PARAMS: -none-
-CREATE FUNCTION new_comments(OUT mime text, OUT js json) AS $$
+CREATE OR REPLACE FUNCTION new_comments(OUT mime text, OUT js json) AS $$
 BEGIN
 	mime := 'application/json';
 	js := json_agg(r) FROM
