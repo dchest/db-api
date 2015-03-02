153145f175e515b4943c4271c28956545f029101
Derek Sivers <derek@sivers.org>
2015-03-03 08:32:22 +1300

    use symbol keys for JSON parsing

--- sivers/test.rb
+++ sivers/test.rb
@@ -21,62 +21,62 @@ class TestComment < Minitest::Test
 			@new_comment[:email],
 			@new_comment[:html]])
 		qry("peeps.get_person(9)")
-		assert_equal 'Bob Dobalina', @j['name']
+		assert_equal 'Bob Dobalina', @j[:name]
 		qry("sivers.get_comment(6)")
-		assert_equal 9, @j['person_id']
-		assert_includes @j['html'], 'þ'
-		refute_includes @j['html'], '<script>'
-		assert_includes @j['html'], '&quot;poop&quot;'
-		refute_includes @j['html'], '<a href'
-		assert_includes @j['html'], 'yuck'
-		assert_includes @j['html'], 'smile.gif'
+		assert_equal 9, @j[:person_id]
+		assert_includes @j[:html], 'þ'
+		refute_includes @j[:html], '<script>'
+		assert_includes @j[:html], '&quot;poop&quot;'
+		refute_includes @j[:html], '<a href'
+		assert_includes @j[:html], 'yuck'
+		assert_includes @j[:html], 'smile.gif'
 	end
 	
 	def test_comments_newest
 		qry("sivers.new_comments()")
-		assert_equal [5, 4, 3, 2, 1], @j.map {|x| x['id']}
+		assert_equal [5, 4, 3, 2, 1], @j.map {|x| x[:id]}
 	end
 
 	def test_reply
 		qry("sivers.reply_to_comment(1, 'Thanks')")
-		assert_equal 'That is great.<br><span class="response">Thanks -- Derek</span>', @j['html']
+		assert_equal 'That is great.<br><span class="response">Thanks -- Derek</span>', @j[:html]
 		qry("sivers.reply_to_comment(2, ':-)')")
-		assert_includes @j['html'], 'smile'
+		assert_includes @j[:html], 'smile'
 	end
 
 	def test_delete
 		qry("sivers.delete_comment(5)")
-		assert_equal 'spam2', @j['html']
+		assert_equal 'spam2', @j[:html]
 		qry("sivers.new_comments()")
-		assert_equal [4, 3, 2, 1], @j.map {|x| x['id']}
+		assert_equal [4, 3, 2, 1], @j.map {|x| x[:id]}
 		qry("peeps.get_person(5)")
-		assert_equal 'Oompa Loompa', @j['name']
+		assert_equal 'Oompa Loompa', @j[:name]
 	end
 
 	def test_spam
 		qry("sivers.spam_comment(5)")
-		assert_equal 'spam2', @j['html']
+		assert_equal 'spam2', @j[:html]
 		qry("peeps.get_person(5)")
 		assert_equal 'application/problem+json', @res[0]['mime']
 		qry("sivers.new_comments()")
-		assert_equal [3, 2, 1], @j.map {|x| x['id']}
+		assert_equal [3, 2, 1], @j.map {|x| x[:id]}
 	end
 
 	def test_update
 		qry("sivers.update_comment(5, $1)", ['{"html":"new body", "name":"Opa!", "created_at":"2000-01-01"}'])
-		assert_equal 'Opa!', @j['name']
-		assert_equal 'new body', @j['html']
-		assert_equal 'oompa@loompa.mm', @j['email']
-		assert_equal '2014-04-28', @j['created_at']
+		assert_equal 'Opa!', @j[:name]
+		assert_equal 'new body', @j[:html]
+		assert_equal 'oompa@loompa.mm', @j[:email]
+		assert_equal '2014-04-28', @j[:created_at]
 	end
 
 	def test_comment_person
 		qry("sivers.get_comment(1)")
-		assert_equal 'trust', @j['uri']
-		assert_equal 'Willy Wonka', @j['person']['name']
-		assert_equal 'musicthoughts', @j['person']['stats'][1]['name']
-		assert_equal 'http://www.wonka.com/', @j['person']['urls'][0]['url']
-		assert_equal 'you coming by?', @j['person']['emails'][0]['subject']
+		assert_equal 'trust', @j[:uri]
+		assert_equal 'Willy Wonka', @j[:person][:name]
+		assert_equal 'musicthoughts', @j[:person][:stats][1][:name]
+		assert_equal 'http://www.wonka.com/', @j[:person][:urls][0][:url]
+		assert_equal 'you coming by?', @j[:person][:emails][0][:subject]
 	end
 
 end
