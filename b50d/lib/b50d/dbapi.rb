7b8e7f22403a50fbc9b46b23b9d9b5f8695bf46d
Derek Sivers <derek@sivers.org>
2015-03-29 06:54:23 +1300

    1.5.0 = PG::Connection replaced with pool: PGP.get

--- b50d/lib/b50d/dbapi.rb
+++ b50d/lib/b50d/dbapi.rb
@@ -1,12 +1,29 @@
 require 'pg'
 require 'json'
 
+# PG Pool of connections. Simple as can be. Bypassed if test database.
+# NOTE: duplicated in db-api/HTTP/
+class PGP
+	@@pool = []
+	@@counter = 0
+	class << self
+		def get(live_or_test='live')
+			if 'test' == live_or_test
+				return PG::Connection.new(dbname: 'd50b_test', user: 'd50b')
+			end
+			my_id = @@counter
+			@@counter += 1
+			@@counter = 0 if 80 == @@counter
+			@@pool[my_id] ||= PG::Connection.new(dbname: 'd50b', user: 'd50b')
+		end
+	end
+end
+
 class DbAPI
 	attr_accessor :error, :message
 
 	def initialize(server='live')
-		dbname = (server == 'test') ? 'd50b_test' : 'd50b'
-		@db =  PG::Connection.new(host: '127.0.0.1', port: 6543, dbname: dbname, user: 'd50b')
+		@db = PGP.get(server)
 	end
 
 	def js(func, params=[])
